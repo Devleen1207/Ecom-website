8321a66d24cf2652e3881805144b794ad6effdaf
Devleen1207 <kourdevleen@gmail.com>
2017-11-10 13:16:03 +0530

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,3 +1,10 @@
 <footer class="navbar navbar-inverse navbar-fixed-bottom">
-	<p style="color:white;  "  class="text-center">Copyright 2017</p>
+	<div class="container-fluid">
+		<ul class="nav navbar-nav">
+			<li><a href="#">Terms & Conditions</a></li>
+			<li><a href="#">About Us</a></li>
+			<li><a href="#">Privacy</a></li>
+			<li><a href="#">Return Policy</a></li>
+		</ul>
+	</div>
 </footer>
